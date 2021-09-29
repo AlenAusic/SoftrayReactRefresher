971aadca742755b5b205cd08dae653ec9764b301
Alen Ausic <alenausic@outlook.com>
2021-09-29 23:19:13 +0200

    Fetching popular languages via GitHub API

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import * as React from 'react'
 import PropTypes from 'prop-types'
+import { fetchPopularRepos } from '../utils/api'
 
-function LanguagesNav ({ selected, onUpdateLanguage }) {
+function LangaugesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
   return (
@@ -10,7 +11,7 @@ function LanguagesNav ({ selected, onUpdateLanguage }) {
         <li key={language}>
           <button
             className='btn-clear nav-link'
-            style={language === selected ? {color: 'rgb(187, 46, 31'} : null }
+            style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
             onClick={() => onUpdateLanguage(language)}>
             {language}
           </button>
@@ -20,7 +21,7 @@ function LanguagesNav ({ selected, onUpdateLanguage }) {
   )
 }
 
-LanguagesNav.propType = {
+LangaugesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
 }
@@ -30,24 +31,55 @@ export default class Popular extends React.Component {
     super(props)
 
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null,
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
+    this.isLoading = this.isLoading.bind(this)
   }
-
-  updateLanguage(selectedLanguage) {
-    this.setState({ selectedLanguage })
+  componentDidMount () {
+    this.updateLanguage(this.state.selectedLanguage)
   }
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+      error: null,
+      repos: null
+    })
 
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) => this.setState({
+        repos,
+        error: null,
+      }))
+      .catch((error) => {
+        console.warn('Error fetching repos: ', error)
+
+        this.setState({
+          error: `There was an error fetching the repositories.`
+        })
+      })
+  }
+  isLoading() {
+    return this.state.repos === null && this.state.error === null
+  }
   render() {
-    const { selectedLanguage } = this.state
+    const { selectedLanguage, repos, error } = this.state
+
     return (
       <React.Fragment>
-        <LanguagesNav
-        selected={selectedLanguage}
-        onUpdateLanguage={this.updateLanguage}
+        <LangaugesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
         />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
       </React.Fragment>
     )
   }
